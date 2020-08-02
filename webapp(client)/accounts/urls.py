edbed881ef5fcc4b9efb60b1a479e1ed24bbedcd
DhyeyaRD <50143762+3DRD@users.noreply.github.com>
2020-08-02 12:09:29 +0530

    Update urls.py

--- webapp(client)/accounts/urls.py
+++ webapp(client)/accounts/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name='accounts'
 urlpatterns=[
 			path('register', views.register, name='register'),
+			path('',views.login,name='login'),
 			path('login', views.login, name='login'),
 			path('logout', views.logout, name='logout'),
 			path('userdetails', views.userDetailsView, name='userdetails'),
